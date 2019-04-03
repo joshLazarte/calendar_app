7b04cb21b280174aeeb1921f9662735282c279d7
Josh <joshualazarte@yahoo.com>
2019-04-02 20:31:29 -0500

    fix update and delete event routes

--- routes/api/event.js
+++ routes/api/event.js
@@ -106,87 +106,97 @@ router.get(
 // @route    PUT /api/event/:id/edit
 // @desc     update an event
 // @access   Private
-router.put('/:id/edit', 
+router.put(
+  "/:id/edit",
   passport.authenticate("jwt", { session: false }),
-  async (req,res) => {
-    
+  async (req, res) => {
     const { errors, isValid } = validateCreateEventInput(req.body);
 
     if (!isValid) {
       return res.status(400).json(errors);
     }
-    
-      try {
-        const currentUser = await utils.eventUtilities.getEventCreatorByUsername(
-          req.user.userName
-        );
-  
-        const foundEvent = await Event.find({
-          createdBy: currentUser,
-          _id: req.params.id
-        });
-  
-        if (isEmpty(foundEvent)) {
-          throw new Error();
-        }
-        
-        const attendeesToAdd = await utils.eventUtilities.getEventAttendees(
-          currentUser,
-          utils.parseStringToBool(req.body.shared),
-          req.body.attendees
-        );
-        
-        foundEvent.name = req.body.name;
-        foundEvent.startDate = req.body.startDate;
-        foundEvent.endDate = req.body.endDate;
-        foundEvent.startTime = req.body.startTime;
-        foundEvent.endTime = req.body.endTime;
-        foundEvent.description = req.body.description;
-        foundEvent.type = req.body.type;
-        foundEvent.location = req.body.location;
-        foundEvent.attendees = attendeesToAdd;
-        foundEvent.shared = req.body.shared;
-        foundEvent.save();
-  
-        res.json(foundEvent);
-      } catch (err) {
-        errors.error = "Unable to edit specified event";
-        res.status(404).json(errors);
-      }
-});
 
-
-// @route    DELETE /api/event/:id/delete
-// @desc     delete an event
-// @access   Private
-router.delete('/:id/delete', 
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const errors = {};
     try {
-      
       const currentUser = await utils.eventUtilities.getEventCreatorByUsername(
         req.user.userName
       );
-      
+
       const foundEvent = await Event.find({
         createdBy: currentUser,
         _id: req.params.id
       });
-      
+
       if (isEmpty(foundEvent)) {
         throw new Error();
       }
-      
-      foundEvent.remove();
-      
-      res.status(200).json({status: 'success'});
-        
-    } catch(err) {
+
+      const attendeesToAdd = await utils.eventUtilities.getEventAttendees(
+        currentUser,
+        utils.parseStringToBool(req.body.shared),
+        req.body.attendees
+      );
+
+      console.log(foundEvent);
+
+      const updatedEventData = {
+        name: req.body.name,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        createdBy: currentUser,
+        startTime: req.body.startTime,
+        endTime: req.body.endTime,
+        description: req.body.description,
+        type: req.body.type,
+        location: req.body.location,
+        attendees: attendeesToAdd,
+        shared: req.body.shared
+      };
+
+      const updatedEvent = await Event.findByIdAndUpdate(
+        req.params.id,
+        updatedEventData
+      );
+
+      res.json(updatedEvent);
+    } catch (err) {
+      errors.error = "Unable to edit specified event";
+      res.status(404).json(errors);
+    }
+  }
+);
+
+// @route    DELETE /api/event/:id/delete
+// @desc     delete an event
+// @access   Private
+router.delete(
+  "/:id/delete",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const errors = {};
+    try {
+      const currentUser = await utils.eventUtilities.getEventCreatorByUsername(
+        req.user.userName
+      );
+
+      const foundEvent = await Event.find({
+        createdBy: currentUser,
+        _id: req.params.id
+      });
+
+      if (isEmpty(foundEvent)) {
+        throw new Error();
+      }
+
+      const deletedEvent = await Event.findOneAndRemove({
+        _id: req.params.id
+      });
+
+      res.status(200).json({ status: "success" });
+    } catch (err) {
       errors.error = "Unable to delete specified event";
       res.status(401).json(errors);
     }
-});
-
+  }
+);
 
 module.exports = router;
