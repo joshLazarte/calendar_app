671c304d282a69cfe81af877f3a6f3938dac534c
Josh <joshualazarte@yahoo.com>
2019-05-18 20:40:18 -0500

    remove tooltips, fix modal

--- client/src/components/events/EventsInCalendarCell.js
+++ client/src/components/events/EventsInCalendarCell.js
@@ -4,14 +4,12 @@ import isEmpty from "../../validation/is-empty";
 import moment from "moment";
 import FormModal from "../modal/FormModal";
 import classNames from "classnames";
-import Tooltip from "../modal/Tooltip";
 
-class EventInCalendarCell extends Component {
+class EventsInCalendarCell extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showModal: false,
-      showTooltip: false,
       renderedMultiDayEvents: [],
       renderedNotMultiDayEvents: [],
       showAllEvents: false,
@@ -51,14 +49,6 @@ class EventInCalendarCell extends Component {
       .format("YYYY-MM-DD");
   };
 
-  showTooltip = () => {
-    this.setState({ showTooltip: true });
-  };
-
-  hideTooltip = () => {
-    this.setState({ showTooltip: false });
-  };
-
   match = (a, b) => a === b;
 
   toggleShowAll = () => {
@@ -68,8 +58,8 @@ class EventInCalendarCell extends Component {
   getHideStart = (multi, single) => {
     let hideStart = false;
 
-    if (!single) return hideStart;
-    if (!multi && single.length > 3) return (hideStart = 3);
+    if (isEmpty(single)) return hideStart;
+    if (isEmpty(multi) && single.length > 3) return (hideStart = 3);
     if (multi.length > 3) return (hideStart = 0);
 
     let j = 0;
@@ -99,8 +89,6 @@ class EventInCalendarCell extends Component {
               { "bg-success": isMulti }
             )}
             onClick={this.showModal(event)}
-            // onMouseEnter={this.showTooltip}
-            // onMouseLeave={this.hideTooltip}
           >
             {this.match(
               this.format(event.startDate),
@@ -109,15 +97,6 @@ class EventInCalendarCell extends Component {
               ? event.name
               : "\u00A0"}
           </a>
-          {this.state.showModal && this.state.eventInModal._id === event._id ? (
-            <FormModal
-              key={event._id + index}
-              disabled={true}
-              eventToDisplay={this.state.eventInModal}
-              hideModal={this.hideModal}
-              formType={"READONLY"}
-            />
-          ) : null}
         </div>
       );
     });
@@ -132,8 +111,6 @@ class EventInCalendarCell extends Component {
     );
     const totalEventsLength =
       renderedMultiDayEvents.length + renderedNotMultiDayEvents.length;
-
-    console.log(hideStart);
 
     let singleEventsToDisplay, arrow;
 
@@ -152,6 +129,7 @@ class EventInCalendarCell extends Component {
         </span>
       );
     } else if (totalEventsLength <= 3) {
+      //@TODO this does not work, shows for multi day events as well
       arrow = null;
     } else {
       arrow = (
@@ -165,16 +143,23 @@ class EventInCalendarCell extends Component {
       <div>
         {renderedMultiDayEvents.map(event => event)}
         {singleEventsToDisplay}
-        {/* {this.state.showTooltip && <Tooltip />} */}
         {arrow}
+        {this.state.showModal ? (
+          <FormModal
+            disabled={true}
+            eventToDisplay={this.state.eventInModal}
+            hideModal={this.hideModal}
+            formType={"READONLY"}
+          />
+        ) : null}
       </div>
     );
   }
 }
 
-EventInCalendarCell.propTypes = {
+EventsInCalendarCell.propTypes = {
   multiDayEvents: PropTypes.array.isRequired,
   notMultiDayEvents: PropTypes.array.isRequired
 };
 
-export default EventInCalendarCell;
+export default EventsInCalendarCell;
