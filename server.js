dda037309aa4e7933f49a84ae27c8921548ca064
Josh <joshualazarte@yahoo.com>
2019-07-29 21:09:41 -0500

    change path to static assests

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use("/api/user", authRoutes);
 app.use("/api/event", eventRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve("./client/build/index.html"));
   });
 }
 
