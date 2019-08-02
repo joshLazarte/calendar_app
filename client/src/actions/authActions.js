fdb77ce39057d00e1eb6d8b5d6b615a26932bd1f
Josh Lazarte <joshualazarte@yahoo.com>
2019-08-02 16:20:24 +0000

    testing new auth route end points

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,7 +4,7 @@ import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 //Register action
 export const registerUser = (user, history) => dispatch => {
   axios
-    .post("/api/user/register", user)
+    .post("/calendar-app/api/user/register", user)
     .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
@@ -17,7 +17,7 @@ export const registerUser = (user, history) => dispatch => {
 //Login action
 export const loginUser = (user, history) => dispatch => {
   axios
-    .post("/api/user/login", user)
+    .post("/calendar-app/api/user/login", user)
     .then(res => {
       localStorage.setItem("user", JSON.stringify(res.data));
       dispatch(setCurrentUser(res.data));
@@ -34,7 +34,7 @@ export const loginUser = (user, history) => dispatch => {
 //Logout action
 export const logoutUser = () => dispatch => {
   axios
-    .delete("/api/user/logout")
+    .delete("/calendar-app/api/user/logout")
     .then(res => {
       localStorage.removeItem("user");
       dispatch(setCurrentUser({}));
