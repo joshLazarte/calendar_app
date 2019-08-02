768f139e61e71fded6d0c4e01900207809ce6e14
Josh Lazarte <joshualazarte@yahoo.com>
2019-08-02 21:04:28 +0000

    edit back link to footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <footer className="bg-dark text-white p-4 mt-auto">
         &copy; {new Date().getFullYear()} 
-        <a href="https://jlazarte.com" className="nav-link text-white">
+        <a href="https://jlazarte.com" className="text-white">
           J3L webDesign
         </a>
       </footer>
