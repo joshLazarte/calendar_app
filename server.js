0ddb55b17d5b17b1e398cf8e0aa30a1c174d7174
Josh <joshualazarte@yahoo.com>
2019-08-02 20:26:31 -0500

    setup api routes for dev and production

--- server.js
+++ server.js
@@ -18,14 +18,17 @@ require("./config/passport")(passport);
 
 const authRoutes = require("./routes/api/auth.js");
 const eventRoutes = require("./routes/api/event.js");
-app.use("/api/user", authRoutes);
-app.use("/api/event", eventRoutes);
 
 if (process.env.NODE_ENV === "production") {
+  app.use("/api/user", authRoutes);
+  app.use("/api/event", eventRoutes);
   app.use(express.static(path.resolve(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve("./client/build/index.html"));
   });
+} else {
+  app.use("/calendar-app/api/user", authRoutes);
+  app.use("/calendar-app/api/event", eventRoutes);
 }
 
 app.listen(process.env.PORT, () =>
